2c10c6b4174056db9ced940273e7e372d600209b
Andres Gutierrez <agfeynman@gmail.com>
2022-06-14 23:43:46 +0100

    feat: show a custom message depending on the clients season

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const seasons = {
+  winter: 'Burr, it is chilly',
+  summer: 'Lets hit the beach',
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+
+  return (
+    <div>
+      <h1>{seasons[season]}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
